d142ee2aea314e1d760f99723363c71f4b580cc6
omer <jagenau49@gmail.com>
2022-07-14 19:47:30 +0200

    checks login value

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import "./App.scss";
 
 function App() {
   const BASE_API_URL =
-    import.meta.env.VITE_BASE_API_URL || "http://localhost:3001";
+    import.meta.env.VITE_BASE_API_URL || "http://localhost:3001";//TODO:change env file to remote
   const [jobSources, setJobSources] = useState([]);
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState({});
   const [inputUsername, setInputUsername] = useState("");
   const [inputPassword, setInputPassword] = useState(""); //TODO:Is it secure?
+  const [loginMessage, setLoginMessage] = useState("");
 
   useEffect(() => {
     if (isUserLoggedIn) {
@@ -24,12 +25,24 @@ function App() {
 
   async function handleLogin(e) {
     e.preventDefault();
-    const _currentUser = (await axios.post(BASE_API_URL + "/login")).data;
-    login(_currentUser);
+
+    await axios
+      .post(BASE_API_URL + "/login", {
+        username: inputUsername,
+        password: inputPassword,
+      })
+      .then((response) => {
+        login(response);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoginMessage("bad login");
+      });
   }
   function login(user) {
     setCurrentUser(user);
     setIsUserLoggedIn(true);
+    setLoginMessage("");
   }
 
   return (
@@ -83,6 +96,7 @@ function App() {
           <div className="row">
             <button>Login</button>
           </div>
+          <p>{loginMessage}</p>
         </form>
       )}
     </div>
