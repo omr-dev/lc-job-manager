82a414b1790d3e71010359402dd8e8ed2a2de400
omer <jagenau49@gmail.com>
2022-07-14 18:48:54 +0200

    dummy login in

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,58 @@
 import axios from "axios";
+
 import { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
-  const API_URL =
-    import.meta.env.VITE_API_URL || "http://localhost:3001/job-sources";
+  const BASE_API_URL =
+    import.meta.env.VITE_BASE_API_URL || "http://localhost:3001";
   const [jobSources, setJobSources] = useState([]);
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
-    (async () => {
-      setJobSources((await axios.get(API_URL)).data);
-    })();
-  }, []);
+    if (isUserLoggedIn) {
+      (async () => {
+        setJobSources((await axios.get(BASE_API_URL + "/job-sources")).data);
+      })();
+    }
+  }, [isUserLoggedIn]);
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    const _currentUser = (await axios.post(BASE_API_URL + "/login")).data;
+    login(_currentUser);
+  }
+  function login(user) {
+    setCurrentUser(user);
+    setIsUserLoggedIn(true);
+  }
+
   return (
     <div className="App">
       <h1>LC Job Manager</h1>
-      <p>There are {jobSources.length} jobs.</p>
-      <ul>
-        {jobSources.map((source, ind) => {
-          return (
-            <li key={ind}>
-              <a href={source.url}>{source.name}</a>
-            </li>
-          );
-        })}
-      </ul>
+      {isUserLoggedIn ? (
+        <>
+          <p>There are {jobSources.length} jobs.</p>
+          <ul>
+            {jobSources.map((source, ind) => {
+              return (
+                <li key={ind}>
+                  <a href={source.url}>{source.name}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      ) : (
+        <form
+          onSubmit={(e) => {
+            handleLogin(e);
+          }}
+        >
+          <button>Login</button>
+        </form>
+      )}
     </div>
   );
 }
